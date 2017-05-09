9d99f64bea4243403143083c4fca280985c19b97
Lucas-Ying <lucas.yingmm@gmail.com>
2017-05-09 19:24:23 +1200

    Fixed navigations display bugs for mobile devices

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,7 +23,35 @@ $(document).ready(function() {
     if (window.location.pathname.substring(0, 5) == "/tag/") {
         $('.panel-cover').addClass('panel-cover--collapsed');
     }
+
+    $('.btn-mobile-menu__icon').click(function() {
+        if ($('.navigation-wrapper').css('display') == "block") {
+            $('.navigation-wrapper').toggleClass('animated bounceOutUp');
+            $('.navigation-wrapper').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+                $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
+            });
+
+        } else {
+            $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
+            $('.navigation-wrapper').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+                $('.navigation-wrapper').toggleClass('animated bounceInDown');
+            });
+        }
+        $('.btn-mobile-menu__icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
+    });
+
+    $('.navigation-wrapper .blog-button').click(function() {
+        if ($('.navigation-wrapper').css('display') == "block") {
+            $('.navigation-wrapper').toggleClass('animated bounceOutUp');
+            $('.navigation-wrapper').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+                $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
+            });
+            $('.btn-mobile-menu__icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
+        }
+    });
+
 });
+
 $(document.links).filter(function() {
     return this.hostname != window.location.hostname;
 }).attr('target', '_blank');
